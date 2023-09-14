64c893f7f0fe7c5e50f029cd961ce030cd74da96
aryanpanwar307 <aryan.panwar.307.ap@gmail.com>
2023-09-14 22:07:00 +0530

    promises

---
+++ promises.js
@@ -0,0 +1,3 @@
+// its a inbuild method in javascript which can be used to handle asynchronous programming
+//you can also make your own promises objects/methods(i dont know what to call just being intellectual :> )
+
